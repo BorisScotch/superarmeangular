0de15e083e8e7415152a755900013575124fa83e
Boris <boris.coalova@gmail.com>
2018-05-27 21:19:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider,  $locationProvider) {
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	})
+	.when('/novedades', {
+		templateUrl: 'pages/novedades.html',
+		controller: 'novedadesController'
+	})
+	.when('/contactanos', {
+		templateUrl: 'pages/contactanos.html',
+		controller: 'contactanosController'
+	})
+	.when('/participa', {
+		templateUrl: 'pages/participa.html',
+		controller: 'participaController'
+	})
+	.when('/galeriadefotos', {
+		templateUrl: 'pages/galeriadefotos.html',
+		controller: 'galeriadefotosController'
+	})
+	.when('/misionyvision', {
+		templateUrl: 'pages/misionyvision.html',
+		controller: 'misionyvisionController'
+	})
+	.when('/equipo', {
+		templateUrl: 'pages/equipo.html',
+		controller: 'equipoController'
+	})
+	.when('/historia', {
+		templateUrl: 'pages/historia.html',
+		controller: 'historiaController'
+	})
+	.when('/testimonios', {
+		templateUrl: 'pages/testimonios.html',
+		controller: 'testimoniosController'
+	})
+	.when('/emprendedores', {
+		templateUrl: 'pages/emprendedores.html',
+		controller: 'emprendedoresController'
+	})
+	 // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+});
+
+
+myApp.controller('mainController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+
+myApp.controller('novedadesController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+
+myApp.controller('contactanosController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+
+myApp.controller('participaController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('galeriadefotosController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('misionyvisionController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('equipoController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('historiaController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('testimoniosController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
+myApp.controller('emprendedoresController', 
+	['$scope', '$log',  function($scope, $log){
+}]);
